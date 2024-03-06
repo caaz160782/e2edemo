103fc6868b97395e0a7c90fc1287e14806b0137d
toño <caaz160782@gmail.com>
2024-03-05 21:11:03 -0600

    e2e test

---
+++ e2e/app.e2e.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const createApp = require('./../src/app');
+
+describe('tests for app', () => {
+
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = createApp();
+    server = app.listen(9000);
+    api = request(app);
+  });
+
+
+  test('GET /hello', async () => {
+    const response = await api.get('/hello');
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.name).toEqual('nico');
+    expect(response.headers['content-type']).toMatch(/json/);
+  })
+
+  afterEach(() => {
+    server.close();
+  })
+});
